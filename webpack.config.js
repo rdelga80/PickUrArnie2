1dea8790bafe8aa134424832828d397c6e0118e1
rdelga80 <ricdelgado80@gmail.com>
2018-01-04 19:42:12 -0500

    Route

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,8 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: 'dist/',
-    filename: '/build.js'
+    publicPath: '/PickUrArnie/dist/',
+    filename: 'build.js'
   },
   module: {
     rules: [
